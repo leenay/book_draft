1325902f02c1eda057f1484420cc15bfd31f41f1
Lee Naylor <Lee.Naylor@open-money.co.uk>
2019-12-01 21:18:26 +0000

    Added seeding and database creation on startup - consider other options for seeding database using ef core and fluent API

---
+++ book_draft.data/Seeding/SeedData.cs
@@ -0,0 +1,42 @@
+﻿using book_draft.domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace book_draft.data.Seeding
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new BankContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<BankContext>>()))
+            {
+                // Look for any movies.
+                if (context.Customers.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Customers.AddRange(
+                    new Customer
+                    {
+                        FirstName = "FirstName1",
+                        MiddleName = "MiddleName1",
+                        LastName = "LastName1"
+                    },
+
+                    new Customer
+                    {
+                        FirstName = "FirstName2",
+                        MiddleName = "MiddleName2",
+                        LastName = "LastName2"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- book_draft/Program.cs
+++ book_draft/Program.cs
@@ -2,8 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using book_draft.data;
+using book_draft.data.Seeding;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +17,28 @@ namespace book_draft
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    using (var context = services.GetService<BankContext>())
+                    {
+                        //create database and apply any migrations
+                        context.Database.Migrate();
+                    }
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the Bank database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
